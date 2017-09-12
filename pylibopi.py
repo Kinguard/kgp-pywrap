1468b96b944c8536178bbd380810d1ea2c29e6b7
PA Nilsson <pa@openproducts.se>
2017-09-12 11:14:25 +0200

    Updated to pass buffert to c++ backend.

--- pylibopi.py
+++ pylibopi.py
@@ -7,35 +7,82 @@ lib = CDLL("libopi_wrapper.so")
 
 def SysTypeText():
 	func = lib.SysTypeText
-	func.restype = c_char_p
-	return func()
+	func.argtypes = [c_char_p]
+	func.restype = c_int
+	length=50 
+	buf = cast(create_string_buffer(length),c_char_p)
+	status = func(buf)
+	if ( status == 1 ):
+		return buf.value.decode("utf-8")
+	else:
+		raise ValueError("Failed to get SystemType from server, status: %s" % status)
+		return false
 
 def SerialNumber():
 	func = lib.SerialNumber
-	func.restype = c_char_p
-	return func()
+	func.argtypes = [c_char_p]
+	func.restype = c_int
+	length=50 
+	buf = cast(create_string_buffer(length),c_char_p)
+	status = func(buf)
+	if ( status == 1 ):
+		return buf.value.decode("utf-8")
+	else:
+		raise ValueError("Failed to get SystemType from server, status: %s" % status)
+		return false
 	
 def StorageDevice():
 	func = lib.StorageDevice
-	func.restype = c_char_p
-	return func()
-	
+	func.argtypes = [c_char_p]
+	func.restype = c_int
+	length=100 
+	buf = cast(create_string_buffer(length),c_char_p)
+	status = func(buf)
+	if ( status == 1 ):
+		return buf.value.decode("utf-8")
+	else:
+		raise ValueError("Failed to get SystemType from server, status: %s" % status)
+		return false
 
 def StorageDeviceBlock():
 	func = lib.StorageDeviceBlock
-	func.restype = c_char_p
-	return func()
+	func.argtypes = [c_char_p]
+	func.restype = c_int
+	length=50 
+	buf = cast(create_string_buffer(length),c_char_p)
+	status = func(buf)
+	if ( status == 1 ):
+		return buf.value.decode("utf-8")
+	else:
+		raise ValueError("Failed to get SystemType from server, status: %s" % status)
+		return false
 
 def StorageDevicePartition():
 	func = lib.StorageDevicePartition
-	func.restype = c_char_p
-	return func()
+	func.argtypes = [c_char_p]
+	func.restype = c_int
+	length=50 
+	buf = cast(create_string_buffer(length),c_char_p)
+	status = func(buf)
+	if ( status == 1 ):
+		return buf.value.decode("utf-8")
+	else:
+		raise ValueError("Failed to get SystemType from server, status: %s" % status)
+		return false
 
 
 def NetworkDevice():
 	func = lib.NetworkDevice
-	func.restype = c_char_p
-	return func()
+	func.argtypes = [c_char_p]
+	func.restype = c_int
+	length=50 
+	buf = cast(create_string_buffer(length),c_char_p)
+	status = func(buf)
+	if ( status == 1 ):
+		return buf.value.decode("utf-8")
+	else:
+		raise ValueError("Failed to get SystemType from server, status: %s" % status)
+		return false
 
 def isArmada():
 	return lib.isArmada(None) > 0
@@ -52,21 +99,42 @@ def isOlimexA20():
 def isPC():
 	return lib.isPC(None) > 0
 
-# For testing only
-if __name__ == '__main__':
-	print( SysTypeText() );
-	print( SerialNumber() );
-	print( StorageDevice() );
-	print( StorageDeviceBlock() );
-	print( StorageDevicePartition() );
-	print( NetworkDevice() );
+
+def AuthLogin():
+	length=100  # token size is set to 50, have some room if we decide to change it.
+	token = cast(create_string_buffer(length),c_char_p)
+	func = lib.Login
+	func.argtypes = [c_char_p]
+	func.restype = c_int
+	status = func(token)
+	if (status != 200 ):
+		raise ValueError("Failed to get token from server, status: %s" % status)
+		return false
+	else:
+		return token.value.decode("utf-8")
 	
 
-	print( isArmada() );
-	print( isOpi() );
-	print( isXu4() );
-	print( isOlimexA20() );
-	print( isPC() );
+# For testing only
+if __name__ == '__main__':
+	print( "SysTypeText: %s" % SysTypeText() );
+	print( "Serialnumber: %s" % SerialNumber() );
+	print( "StorageDevice: %s" % StorageDevice() );
+	print( "StorageDeviceBlock: %s" % StorageDeviceBlock() );
+	print( "StorageDevicePartition: %s" % StorageDevicePartition() );
+	print( "NetworkDevice: %s" % NetworkDevice() );
+	
+
+	print( "isArmada: %s" % isArmada() );
+	print( "isOpi: %s" % isOpi() );
+	print( "isXu4: %s" % isXu4() );
+	print( "isOlimexA20: %s" % isOlimexA20() );
+	print( "isPC: %s" % isPC() );
    
-   
+	try:
+		token=AuthLogin()
+		print("Token %s\n" % token )
+	except ValueError as e:
+		print(e.args)
+		
+	   
 
